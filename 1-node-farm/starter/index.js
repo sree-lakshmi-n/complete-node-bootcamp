f1039f12e6bb8d1142281769c48dae6389e3fa80
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-02 07:19:38 +0530

    Hello World. Blocking way of reading/writing file. Non-blocking way of reading/writing file. Callback Hell.

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,36 @@
+// const hello = "Hello World";
+// console.log(hello);
+
+const fs = require("fs");
+// Blocking, Synchronous way
+// To read a text file into a variable
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// // To output into a text file
+// const textOut = `This is what we know about the avocado: ${textIn}\n Created on ${Date.now()}`;
+// // Writing textOut to a new file
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("text written!");
+
+// Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
+console.log("Will read file ...");
+
+// Nested callback file read (Callback hell)
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR! 💥");
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+
+      // Writing to a new file
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has been written. 😇");
+      });
+    });
+  });
+});
