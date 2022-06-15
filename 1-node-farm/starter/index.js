2b6a040a67a4a087d10e32e81af024a3155f5f7f
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-15 18:48:37 +0530

    set content type to html

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -31,6 +31,9 @@ const server = http.createServer((req, res) => {
 
   // Overview page
   if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
     res.end(tempOverview);
   }
 });
