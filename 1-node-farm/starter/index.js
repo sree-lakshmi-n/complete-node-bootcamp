b123a0da4c75fb417c0fa8823379a3c48bae91e6
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-16 16:15:27 +0530

    Took out replaceTemplate function to be placed in a separate module

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -13,21 +13,6 @@ const url = require("url");
 // In dataObject, we have an array of all the objects that are in data.JSON.
 //  We have to loop through this array, and for each of them, replace the placeholders in the template with the actual data from the current product.
 
-// function to replace template
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "");
-  else output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
-
 // Read templates
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
