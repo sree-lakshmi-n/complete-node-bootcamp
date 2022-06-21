901457a36e51ca13b8a34dfbf592f9ef96a0714f
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-21 14:38:48 +0530

    Reading a big file: Solution 1

--- 2-how-node-works/starter/streams.js
+++ 2-how-node-works/starter/streams.js
@@ -3,4 +3,11 @@ const fs = require("fs");
 const server = require("http").createServer();
 
 // listening to requests
-server.on("request", (req, res) => {});
+server.on("request", (req, res) => {
+    // We've to read a big file
+  // Solution 1
+  fs.readFile("test-file.txt", (err, data) => {
+    if (err) console.log(err);
+    res.end(data);
+  });
+});
