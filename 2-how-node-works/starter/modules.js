4f53c8bd129ca8eefed02f6b49d0c2be2e9f3da3
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-21 17:55:40 +0530

    Proving that we're inside wrapper function by checking if there are valuesin arguments array

---
+++ 2-how-node-works/starter/modules.js
@@ -0,0 +1,4 @@
+// Proving that we're inside wrapper function
+// If arguments array isn't empty, => we're inside a function
+// Since, arguments array contains all the values that are passed into a function
+console.log(arguments);
