6bc07a00321963f3b0d36ca8072299d04d8cd085
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-21 18:31:02 +0530

    Commenting out class declaration and exporting

--- 2-how-node-works/starter/test-module1.js
+++ 2-how-node-works/starter/test-module1.js
@@ -1,14 +1,15 @@
-class Calculator {
-  add(a, b) {
-    return a + b;
-  }
-  multiply(a, b) {
-    return a * b;
-  }
-  divide(a, b) {
-    return a / b;
-  }
-}
+// CLASS DECLARATION
+// class Calculator {
+//   add(a, b) {
+//     return a + b;
+//   }
+//   multiply(a, b) {
+//     return a * b;
+//   }
+//   divide(a, b) {
+//     return a / b;
+//   }
+// }
 
 // Exporting Calculator class
 module.exports = Calculator;
