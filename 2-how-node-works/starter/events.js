efeb1aa9b3a9cb18a8730d3b38e9a1f5d1acdf22
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-21 13:10:58 +0530

    Multiple listeners for the same event

--- 2-how-node-works/starter/events.js
+++ 2-how-node-works/starter/events.js
@@ -3,9 +3,14 @@ const EventEmitter = require("events");
 // Instance of EventEmitter class
 const myEmitter = new EventEmitter();
 
-// Listening to an event
+// Listening to an event - Observer Pattern
 myEmitter.on("newSale", () => {
   console.log("There was a new event!");
+});
+
+// Multiple listeners for the same event
+myEmitter.on("newSale", () => {
+  console.log("Customer name: Jane");
 });
 
 // Emitting an event
