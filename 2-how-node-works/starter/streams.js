6d1bb73a302533db1ad0f10c1153fb8132a0f5ad
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-21 15:59:32 +0530

    Reading a big file using streams

--- 2-how-node-works/starter/streams.js
+++ 2-how-node-works/starter/streams.js
@@ -6,9 +6,21 @@ const server = require("http").createServer();
 server.on("request", (req, res) => {
   // We've to read a big file
   // Solution 1 - Not apt for bigger projects/ productions
-  fs.readFile("test-file.txt", (err, data) => {
-    if (err) console.log(err);
-    res.end(data);
+  //   fs.readFile("test-file.txt", (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  //
+  ////////////////////////////
+  // Solution 2 - Streams
+  //We don't need data variable. Instead of reading the data into a variable, and having to store that variable into memory, we will just create a readable stream.
+  // Then as we receive each chunk of data, we send it to the client as a response which is a writable stream.
+  const readable = fs.createReadStream("test-file.txt");
+  //Each time there is a new piece of data that we can consume, a readable stream emits the data event.
+  readable.on("data", (chunk) => {
+    res.write(chunk);
+  });
+  readable.on("end", () => {
+    res.end();
   });
 });
 
