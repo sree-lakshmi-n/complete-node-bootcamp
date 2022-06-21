3b012fbeb2e1856bd6564e123591238e5b09bb46
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-21 18:49:31 +0530

    Using exports

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -6,9 +6,14 @@ console.log(arguments);
 // 'module' module
 console.log(require("module").wrapper);
 
+// module.exports
 // Importing test-module1 (Calculator class)
 const C = require("./test-module1");
 // Creating an instance of Calculator class
 const calc1 = new C();
 
 console.log(calc1.add(3, 4));
+
+// exports
+const calc2 = require("./test-module2");
+console.log(calc2.add(3, 4));
