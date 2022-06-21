1ff9e7d268feaa911d23896ca6db07826e33dd44
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-21 13:08:54 +0530

    Listening to an event

--- 2-how-node-works/starter/events.js
+++ 2-how-node-works/starter/events.js
@@ -3,5 +3,10 @@ const EventEmitter = require("events");
 // Instance of EventEmitter class
 const myEmitter = new EventEmitter();
 
+// Listening to an event
+myEmitter.on("newSale", () => {
+  console.log("There was a new event!");
+});
+
 // Emitting an event
 myEmitter.emit("newSale");
