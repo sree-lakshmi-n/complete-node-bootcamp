650846290ba99269bfb1d77c50ca4fb0942abee7
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-22 16:25:10 +0530

    Promisified read and write fn

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -21,8 +21,28 @@ const superagent = require("superagent");
 // });
 
 // PROMISES
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  if (err) console.log(err);
+// Building promises
+// read fn
+const fileReadPro = (file) => {
+  // Promise Constructor
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("Could not find file 😢");
+      resolve(data);
+    });
+  });
+};
+// write fn
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("Could not find file 😢");
+      resolve("Success!");
+    });
+  });
+};
+
+fileReadPro(`${__dirname}/dog.txt`).then((data) => {
   console.log(`Breed: ${data}`);
   // Getting a random dog image of the given breed
   superagent
