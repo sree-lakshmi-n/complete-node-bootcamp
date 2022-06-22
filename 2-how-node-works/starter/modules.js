d832abb21a42972b624f09db10411738307c454f
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-06-22 12:26:33 +0530

    Caching demo

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -21,3 +21,26 @@ console.log(calc1.add(3, 4));
 const { add, multiply, divide } = require("./test-module2");
 // We can also import only the ones we want
 console.log(add(3, 4));
+
+// caching
+require("./test-module3")();
+require("./test-module3")();
+require("./test-module3")();
+
+// Output of caching demo:
+/* 
+Hello from the module!
+Log this beautiful text 😁
+Log this beautiful text 😁
+Log this beautiful text 😁
+ */
+/*
+'Log this beautiful text 😁' comes three times because we called the same function three times. But we have hello from the module only once.
+And that is because of caching.
+
+Technically this module was only loaded once, and so the code inside of it was also executed once only. And so that's why 'Hello from the module!' was run only once.
+
+'Log this beautiful text 😁' came from cache, so they were stored somewhere in the Node's processes cache.
+
+And once we called the function for the second time, it was simply retrieved from there, instead of loading the module again, 
+*/
