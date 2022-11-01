b11bd149e84266fa3e32569217c3fc99579bc53d
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-11-01 14:58:00 +0530

    Reading start.txt

--- 1-node-farm/starter/file-system.js
+++ 1-node-farm/starter/file-system.js
@@ -1,8 +1,14 @@
 const fs = require("fs");
 
+// Blocking, synchronous way
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
 
 const textOut = `This is what we know about avocado: ${textIn}. \n Created on ${Date.now().toLocaleString()}`;
 fs.writeFileSync("./txt/output.txt", textOut);
 console.log("File written!");
+
+// Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
