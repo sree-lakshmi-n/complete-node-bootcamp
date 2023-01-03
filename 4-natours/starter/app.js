f1fe23dacf089e7ae5bc3ed9c24b59bbfafd570d
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-03 15:15:50 +0530

    handling GET requests

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
-app.get('/', (req, res) => {
-  //   res.status(200).send('Hello from the server side!');
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
-app.post('/', (req, res) => {
-  res.end('You can post to this endpoint...');
+// app.get('/', (req, res) => {
+//   //   res.status(200).send('Hello from the server side!');
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+// app.post('/', (req, res) => {
+//   res.end('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).send({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+      //  or tours:tours
+    },
+  });
 });
 
 const port = 3000;
