19ccae6cb9624b3d8fef0c00e9607ab7f145f462
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-03 17:14:29 +0530

    handling post requests

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
+// Using middleware
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   //   res.status(200).send('Hello from the server side!');
 //   res
@@ -25,6 +28,31 @@ app.get('/api/v1/tours', (req, res) => {
       //  or tours:tours
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  // // body is the property that is gonna be available on the request, because we used the middleware express.json()
+  // res.end('Done');
+  // //  we always need to send back something in order to finish the request/response cycle,
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      // 200 - okay
+      // 201 - created
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
