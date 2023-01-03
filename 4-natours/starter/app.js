ba70d168938d0562aec515a402a2401b35adbfa9
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-03 05:59:28 +0530

    basic routing with express

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  //   res.status(200).send('Hello from the server side!');
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+app.post('/', (req, res) => {
+  res.end('You can post to this endpoint...');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log('listening to port: ', port);
+});
